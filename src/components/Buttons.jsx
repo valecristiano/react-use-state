b5030158b73fb70d4682fa8a73f0863fcd49ff16
valecristiano <valeriamdcristiano@gmail.com>
2026-02-09 20:10:34 +0300

    clicked button yellow

--- src/components/Buttons.jsx
+++ src/components/Buttons.jsx
@@ -8,11 +8,14 @@ export default function Buttons({ language }) {
     <>
       <section className="container">
         <div className="d-flex justify-content-around gap-4 m-5">
-          {language.map((language) => (
-            <button key={language.id} className="btn btn-primary" onClick={() => setClicked(clicked === language ? null : language)}>
-              {language.title}
-            </button>
-          ))}
+          {language.map((language) => {
+            const isActive = clicked === language;
+            return (
+              <button key={language.id} className={isActive ? "btn btn-warning" : "btn btn-primary"} onClick={() => setClicked(clicked === language ? null : language)}>
+                {language.title}
+              </button>
+            );
+          })}
         </div>
         {clicked ? <DescriptionCard title={clicked.title} description={clicked.description}></DescriptionCard> : <div className="card p-4">Nessun linguaggio selezionato </div>}
       </section>
